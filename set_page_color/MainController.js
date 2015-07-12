e8f185ed82b591b19a5f288d45fdfaa0c97f56b4
Eduardo Ramirez <eduardo@ucsd.edu>
2015-07-11 23:39:03 -0700

    new updates to sidebar

--- set_page_color/MainController.js
+++ set_page_color/MainController.js
@@ -1,11 +1,30 @@
+var tagName
+var tagText
+
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
   if (msg.text && (msg.text == "report_back")) {
     if (!$('#__teamthebest__').length) {
+      console.log(">>>")
       buildSidebar(document.body)
     }
+    else {
+      console.log("<<<<<")
 
-   $('.sidebar').sidebar('toggle');
+    }
+
+    $('#__teamthebest__').sidebar('toggle');
+
+    setTimeout(function () {
+      $('.pusher').removeClass('dimmed')
+    }, 10);
+
+
+    $("*", document.body).click(function (e) {
+      e.stopPropagation();
+      tagName = $(this).get(0).tagName;
+      tagText = $(this).get(0).innerText;
+    })
   }
 })
 
@@ -16,11 +35,18 @@ function buildSidebar(body) {
   document.body.innerHTML = newBody
 }
 
-
 function populateSidebar() {
   var sidebar = '<div class="ui sidebar" id="__teamthebest__">'
+  
+  var stuff =
+    '<h1 class="ui header">Readmix</h1>' +
+      '<form class="ui form">' +
+        '<div class="field">' +
+          '<label>Enter New Text Below:</label>' +
+          '<input type="text" name="new-text" placeholder='+ tagName + ' />' +
+        '</div>' +
+        '<div class="ui inverted blue button">Submit</div>'+
+      '</form>'
 
-  // fill in sidebar html here
-
-  return sidebar + '</div>'
+  return sidebar + stuff + '</div>'
 }
