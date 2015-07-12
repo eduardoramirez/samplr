7899503386c8b536bd66b1b87dcbfd2c9b7bcbd2
CarlaCastro <ccastro6@jhu.edu>
2015-07-11 23:30:36 -0700

    first part of sidebar

--- set_page_color/MainController.js
+++ set_page_color/MainController.js
@@ -1,4 +1,14 @@
+var tagName
+var tagText
+
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+  $("*", document.body).click(function (e) {
+  	e.stopPropagation();
+  	tagName = $(this).get(0).tagName;
+  	tagText= $(this).get(0).innerText;
+  	});
+
 
   if (msg.text && (msg.text == "report_back")) {
     if (!$('#__teamthebest__').length) {
@@ -7,15 +17,15 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
    $('.sidebar').sidebar('toggle');
   }
-$("*", document.body).click(function (e) {
-	e.stopPropagation();
-	var tagName = $(this).get(0).tagName;
-	var tagText = $(this).get(0).innerText;
-	});
+
 })
+
 
 function buildSidebar(body) {
   var myDiv = populateSidebar()
+
+  console.log(myDiv);
+
   var newBody = myDiv + '<div class="pusher">' + body.innerHTML + '</div>'
 
   document.body.innerHTML = newBody
@@ -23,10 +33,17 @@ function buildSidebar(body) {
 
 
 function populateSidebar() {
-  var sidebar = '<div class="ui sidebar" id="__teamthebest__">'
+  var sidebar = '<div class="ui sidebar" id="__teamthebest__">';
 
-  // fill in sidebar html here
+  var stuff =
+    '<h1 class="ui header">Readmix</h1>' +
+      '<form class="ui form">' +
+        '<div class="field">' +
+          '<label>Enter New Text Below:</label>' +
+          '<input type="text" name="new-text" placeholder='+ tagName + '>' +
+        '</div>' +
+        '<div class="ui inverted blue button">Submit</div>'+
+      '</form>';
 
-  return sidebar + '</div>'
+  return sidebar + stuff + '</div>'
 }
-
