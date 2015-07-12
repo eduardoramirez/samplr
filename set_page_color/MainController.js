3b5971ba9295ef92e0c0ea77368e5c2758ed08cc
Jordan <powellj@mit.edu>
2015-07-12 03:00:54 -0400

    Basic sidebar functionality

--- set_page_color/MainController.js
+++ set_page_color/MainController.js
@@ -1,5 +1,6 @@
 var tagName
 var tagText
+var element
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
@@ -20,8 +21,12 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
 $("*", document.body).click(function (e) {
 	e.stopPropagation();
-	var tagName = $(this).get(0).tagName;
-	var tagText = $(this).get(0).innerText;
+	tagName = $(this).get(0).tagName;
+	tagText = $(this).get(0).innerText;
+	element = $(this);
+	$("#textField").attr("placeholder", tagText)
+	$("#topTag").text("<"+tagName.toLowerCase()+">");
+	$("#bottomTag").text("</"+tagName.toLowerCase()+">");
 	console.log('<'+tagName.toLowerCase()+'>'+tagText+'<'+tagName.toLowerCase()+'/>');
 	});
 })
@@ -36,14 +41,14 @@ function buildSidebar(body) {
 
 function populateSidebar() {
   var sidebar = '<div class="ui sidebar" id="__teamthebest__">';
-
   var stuff =
     '<h1 class="ui header">Readmix</h1>' +
-      '<form class="ui form">' +
+      '<form class="ui form">'+
+      	'<h3 id="topTag">'+tagName+'</h3>' +
         '<div class="field">' +
-          '<label>Enter New Text Below:</label>' +
-          '<input type="text" name="new-text" placeholder='+ tagName + '>' +
+          '<input id="textField" type="text" name="new-text" placeholder='+ tagName + '>' +
         '</div>' +
+        '<h3 id="bottomTag">'+tagName+'</h3>'+
         '<div class="ui inverted blue button">Submit</div>'+
       '</form>';
 
